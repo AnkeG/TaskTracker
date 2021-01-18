7f60ff57fe2a4008e16331af07c3562bd47de5d3
Anke Ge <geanke12@gmail.com>
2021-01-18 01:13:59 -0800

    add task log

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 tracking varies tasks or projects and generates reports of everyday
 
 To-do:
-1) Replacing start button with least six "activity" buttons
-2)* add icons to activity
-3) build file system that records date, activities and time spend of each
+1) Replacing start button with least six "tasks" buttons （* add icons on the buttons）
+2) built a task log with tk.text widget
+3) add a file system that records date, activities and time spend of each
 4) GUI for daily activity report
 5)* charts for report
 6) improve GUI
--- tracker/tracker.py
+++ tracker/tracker.py
@@ -9,7 +9,7 @@ def updatewatch():
 	now = time.time()
 	delta = now-startstamp
 	string = time.strftime("%H:%M:%S",time.gmtime(delta))
-	watch['text'] = str(string)
+	watch['text'] = string
 	if run:
 		watch.after(1000, updatewatch)
 
@@ -18,28 +18,39 @@ def start():
 	tstamp = time.time()
 	startstamp = tstamp
 	run = True
+	start_btn['state'] = "disable"
+	stop_btn['state'] = "normal"
 	updatewatch()
 
 def stop():
 	global startstamp, run
 	tstamp = time.time()
+	endtime = time.strftime("%m/%d/%y %H:%M", time.localtime(tstamp))
+	starttime = time.strftime("%m/%d/%y %H:%M", time.localtime(startstamp))
+	print(endtime, starttime)
+	tasklogs.insert(tk.END, starttime+' ~ '+endtime+'\n')
 	startstamp = tstamp
 	run = False
+	start_btn['state'] = "normal"
+	stop_btn['state'] = "disable"
 
 window = tk.Tk()
 window.title("Task Tacker")
 
-window.minsize(width = 300, height = 300)
+window.minsize(width = 100, height = 130)
 
 watch =  tk.Label(window, text = "00:00:00", font="TimeNew 30 bold")
+tasklogs = tk.Text(window, height = 15, width=50)
+tasklogs.insert(tk.END, "Tasks Logs\n")
 btn_frame = tk.Frame(window)
 start_btn = tk.Button(btn_frame, text = "start", command = start)
-stop_btn = tk.Button(btn_frame, text = "stop", command = stop)
+stop_btn = tk.Button(btn_frame, text = "stop", command = stop, state = "disable")
 
 start_btn.grid(row = 0, column = 0, padx = 10)
 stop_btn.grid(row = 0, column = 1, padx = 10)
 
 watch.pack()
 btn_frame.pack()
+tasklogs.pack(pady = 10)
 
 window.mainloop()
