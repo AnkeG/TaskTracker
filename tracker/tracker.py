888bc6dd8f827cbbf603d412afc2e4613b09486d
Anke Ge <geanke12@gmail.com>
2021-01-24 11:38:31 -0800

    fix bugs, add percentages next to pie chart

--- tracker/tracker.py
+++ tracker/tracker.py
@@ -26,12 +26,15 @@ def start(t):
 	starttime = time.strftime("%m/%d/%y %H:%M", time.localtime(startstamp))
 	runtask = t
 
+	tasklogs['state'] = 'normal'
 	tasklogs.insert(tk.END, t+': '+starttime+' ~ ')
+	tasklogs['state']="disable"
 
 	for task in tasks:
-		if task.name == runtask:
-			task.btn['text'] = 'stop'
+		if task.name == t:
+			task.btn['text'] = 'STOP'
 			task.btn['command'] = stop
+			task.btn['font'] = "TimeNew 11 bold"
 		else:
 			task.btn['state'] = "disable"
 	report_btn['state'] = 'disable'
@@ -50,13 +53,16 @@ def stop():
 	span = time.strftime("%H:%M", time.gmtime(tspan))
 	totalspan += tspan
 
+	tasklogs['state'] = 'normal'
 	tasklogs.insert(tk.END, endtime+'  '+span+'\n')
+	tasklogs['state']="disable"
 
 	for task in tasks:
 		if task.name == temptask:
-			task.span = tspan
+			task.span += tspan
 			task.btn['text'] = task.name
-			task.btn['command'] = command = lambda t=t: start(t.name)
+			task.btn['command'] = lambda t=t: start(temptask)
+			task.btn['font'] = "TimeNew 11"
 		task.btn['state'] = "normal"
 	report_btn['state'] = 'normal'
 
@@ -70,7 +76,6 @@ def save():
 	tasklogs['state']="disable"
 
 def report():
-	tasklogs['state']="disable"
 	reportwindow.deiconify()
 
 def close():
@@ -79,8 +84,10 @@ def close():
 def updatepie():
 	lastend = 0
 	for task in tasks:
-		extent = task.span/totalspan*359.99
+		percentage = task.span/totalspan
+		extent = percentage*359.99
 		pie.itemconfig(task.pieslice, start = lastend, extent = extent)
+		task.percent['text'] = str(round(percentage*100))+'%'
 		lastend += extent
 
 if __name__ == '__main__':
@@ -104,9 +111,9 @@ if __name__ == '__main__':
 
 	report_btn = tk.Button(window, text = "Statistic Summary", command = report)
 
-	btn_frame.pack()
-	watch.pack(pady = 20)
-	report_btn.pack()
+	btn_frame.pack(pady = 10)
+	watch.pack(pady = 10)
+	report_btn.pack(pady = 10)
 
 #report window
 	reportwindow = tk.Tk()
@@ -124,16 +131,24 @@ if __name__ == '__main__':
 	tasklogs = tk.Text(reportwindow, height = 15, width=50)
 	tasklogs.insert(tk.END, "Tasks Logs\n")
 
+	pielabels = tk.Frame(reportwindow)
 	pie = tk.Canvas(reportwindow, height = 230, width = 260)
 	coord = 10, 20, 250, 210
 	oval = pie.create_oval(coord)
-	for t in tasks:
+	for i,t in enumerate(tasks):
 		t.pieslice = pie.create_arc(coord, start = 0, extent = 0, 
 			fill = t.color, outline = t.color)
+		label = tk.Label(pielabels, text = t.name, 
+			font = "Calibri 13 bold", fg = t.color)
+		label.grid(row = i, column = 0, padx = 5, pady = 3)
+		t.percent = tk.Label(pielabels, text = '0%', 
+			font = "Calibri 13 bold", fg = t.color)
+		t.percent.grid(row = i, column = 1, padx = 5, pady = 3)
 
 	reportbtns.pack(side = tk.RIGHT, pady = 10, fill=tk.BOTH)
 	tasklogs.pack(expand = True, fill = tk.BOTH)
-	pie.pack()
+	pie.pack(side = tk.LEFT)
+	pielabels.pack()
 
 	reportwindow.withdraw()
 
